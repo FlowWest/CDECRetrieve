52d366049bb592aec00dc9e9f0a39333e94d75c0
ergz <e.emanuel.rodriguez@gmail.com>
2017-02-27 23:31:42 -0800

    improved queries by using shef retrieval service from cdec

--- R/consts.R
+++ R/consts.R
@@ -3,3 +3,7 @@
 # Date:
 # Description: A location to place all global and constant values
 # ----------------------
+
+cdec_urls <- list(
+  download_shef = "https://cdec.water.ca.gov/cgi-progs/querySHEF?station_id=STATION&sensor_num=SENSOR&dur_code=DURCODE&start_date=STARTDATE&end_date=ENDDATE&data_wish=Download+SHEF+Data+Now"
+)
