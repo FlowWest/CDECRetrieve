dd70c7c0272e76cdf88b022372b06db955f10b43
ergz <e.emanuel.rodriguez@gmail.com>
2017-03-27 15:25:22 -0700

    ported over get_station_metadata from old package

--- R/get_station_metadata.R
+++ R/get_station_metadata.R
@@ -1,3 +1,22 @@
+source("R/consts.R")
+
+#' Function helper to get_station_metadata below
+parse_attr_to_df <- function(resp) {
+  parsed_resp <- data.frame(station_id=resp[1,2],
+                            elevation=readr::parse_number(resp[1,4]),
+                            river_basin=tolower(resp[2,2]),
+                            county=resp[2,4],
+                            hydrologic_area=tolower(resp[3,2]),
+                            nearby_city=tolower(resp[3,4]),
+                            latitude=readr::parse_number(resp[4,2]),
+                            longitude=readr::parse_number(resp[4,4]),
+                            operator=tolower(resp[5,2]),
+                            data_collection=tolower(resp[5,4]),
+                            stringsAsFactors = FALSE)
+
+  return(parsed_resp)
+}
+
 #' Use three letter code to retieve associated attributes
 #'
 #' @param stations a vector of three letter station codes.
@@ -6,25 +25,13 @@
 get_station_metadata <- function(stations) {
 
   map(stations, function(station) {
-    httr::GET(req_urls$station_metadata_url,
+    httr::GET(cdec_urls$station_metadata,
               query = list(station_id=station)) %>%
       xml2::read_html() %>%
       rvest::html_node("table") %>%
       rvest::html_table()
   }) %>%
     map(parse_attr_to_df) %>%
-    bind_rows()
+    dplyr::bind_rows()
 
-  # depecrated ------------------------------------------
-  # for (station in stations) {
-  #   res <- httr::GET(req_urls$station_metadata_url,
-  #                    query = list(station_id=station))
-  #   station_attributes <-
-  #     xml2::read_html(res) %>%
-  #     rvest::html_node("table") %>%
-  #     rvest::html_table() %>%
-  #     parse_attr_to_df()
-  #
-  #   station_res[[station]] <- station_attributes
-  # }
 }
