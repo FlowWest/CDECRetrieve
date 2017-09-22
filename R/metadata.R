f63d37e59fc4cbc450831d254accb734a264dd3f
ergz <erodriguez@flowwest.com>
2017-09-22 13:43:34 -0700

    refactor names of functions to be more how should i say not a sentence long
    some of the functions have been renamed to shorter names and deprecated messages added to old ones
    * retrieve_station_data() --> query()
    * get_station_metadata() --> query_metadata()

---
+++ R/metadata.R
@@ -0,0 +1,50 @@
+#' Use three letter code to retieve associated attributes. NOTE: function
+#' deprecated use 'query_metadata()'.
+#'
+#' @param stations a vector of three letter station codes.
+#' @return a list of attributes associated with each station in the \code{stations} vector
+#' @export
+get_station_metadata <- function(stations) {
+
+  .Deprecated("query_metadata", package = "CDECRetrieve",
+              msg = "function deprecated, use 'query_metadata()'")
+
+  query_metadata(stations)
+
+}
+
+#' Use three letter code to retieve associated attributes
+#'
+#' @param stations a vector of three letter station codes.
+#' @return a dataframe with rows as stations submitted to the query
+#' @export
+query_metadata <- function(stations) {
+  resp <- purrr::map_dfr(stations, ~call_cdec_meta_service(.))
+  parse_meta_response(resp)
+}
+
+# Helpers
+
+call_cdec_meta_service <- function(station) {
+  cdec_urls$station_metadata %>%
+    stringr::str_replace("STATION", station) %>%
+    xml2::read_html() %>%
+    rvest::html_table() %>%
+    purrr::flatten()
+}
+
+parse_meta_response <- function(.d) {
+  .d %>%
+    dplyr::mutate_(
+      agency = lazyeval::interp(~x, x="cdec"),
+      state = lazyeval::interp(~x, x="ca"),
+      location_id = ~tolower(ID),
+      location_name = ~tolower(`Station Name`),
+      county = ~tolower(County)
+    ) %>%
+    dplyr::select_(
+      "agency", "location_id", "location_name",
+      "county", lat = "Latitude",
+      long = "Longitude", "state"
+    )
+}
---
+++ R/query.R
@@ -0,0 +1,93 @@
+#' Function queries the CDEC services to obtain desired station data
+#' @param station_id three letter identification for CDEC location.
+#' @param sensor_num sensor number for the measure of interest.
+#' @param dur_code duration code for measure interval, "E", "H", "D", which correspong to Event, Hourly and Daily.
+#' @param start_date date to start the query on.
+#' @param end_date a date to end query on, defaults to current date.
+#' @return tidy dataframe
+#' @examples
+#' kwk_hourly_temp <- CDECRetrieve::retrieve_station_data("KWK", "20", "H", "2017-01-01")
+#'
+#' @export
+retrieve_station_data <- function(station_id, sensor_num,
+                                dur_code, start_date, end_date="") {
+
+  .Deprecated("query", package = "CDECRetrieve",
+              msg = "function has been deprecated use 'query()'")
+
+  query(station_id, sensor_num,
+        dur_code, start_date, end_date="")
+}
+
+
+#' Function queries the CDEC services to obtain desired station data
+#' @param station_id three letter identification for CDEC location.
+#' @param sensor_num sensor number for the measure of interest.
+#' @param dur_code duration code for measure interval, "E", "H", "D", which correspong to Event, Hourly and Daily.
+#' @param start_date date to start the query on.
+#' @param end_date a date to end query on, defaults to current date.
+#' @return tidy dataframe
+#' @examples
+#' kwk_hourly_temp <- CDECRetrieve::retrieve_station_data("KWK", "20", "H", "2017-01-01")
+#'
+#' @export
+query <- function(station_id, sensor_num,
+                  dur_code, start_date, end_date="") {
+  # a real ugly side effect here, but its reliability is great
+  raw_file <- utils::download.file(make_cdec_url(station_id, sensor_num,
+                                                 dur_code, start_date, end_date),
+                                   destfile = "tempdl.txt",
+                                   quiet = TRUE)
+
+  # catch the case when cdec is down
+  if (file.info("tempdl.txt")$size == 0) {
+    stop("query did not produce a result, possible cdec is down?")
+  }
+
+  on.exit(file.remove("tempdl.txt"))
+  resp <- shef_to_tidy("tempdl.txt")
+  resp$agency_cd <- "CDEC"
+  resp[,c(5, 1:4)]
+}
+
+
+# Helpers
+
+make_cdec_url <- function(station_id, sensor_num,
+                          dur_code, start_date, end_date=as.character(Sys.Date())) {
+  cdec_urls$download_shef %>%
+    stringr::str_replace("STATION", station_id) %>%
+    stringr::str_replace("SENSOR", sensor_num) %>%
+    stringr::str_replace("DURCODE", dur_code) %>%
+    stringr::str_replace("STARTDATE", start_date) %>%
+    stringr::str_replace("ENDDATE", end_date)
+
+}
+
+#' Function converts shef downloaded data into one of a tidy format
+#' @param file shef filename obtained from CDEC
+#' @return data frame in tidy form
+shef_to_tidy <- function(file) {
+  raw <- readr::read_delim(file, skip = 9, col_names = FALSE, delim = " ")
+
+  if (ncol(raw) < 5) {
+    stop("A faulty query was requested, please check query,
+         does this station have this duration and sensor combination?")
+  }
+
+  raw <- raw[, c(2, 3, 5, 6, 7)]  # keep relevant cols
+  raw <- raw %>% tidyr::unite_(col = "datetime",
+                               from = c("X3", "X5"), sep ="", remove = TRUE)
+  raw$datetime <- lubridate::ymd_hm(raw$datetime)
+
+  shef_code <- raw$X6[1]
+  cdec_code <- ifelse(is.null(shef_code_lookup[[shef_code]]),
+                      NA, shef_code_lookup[[shef_code]])
+  raw$X6 <- rep(cdec_code, nrow(raw))
+  colnames(raw) <- c("location_id", "datetime", "parameter_cd", "parameter_value")
+
+  # parse to correct type
+  raw$parameter_value <- as.numeric(raw$parameter_value)
+
+  return(raw[, c(2, 1, 3, 4)])
+}
