c6138afbb9a5d1ab2ed0d1bc859d0ae006e3cb7e
ergz <erodriguez@flowwest.com>
2017-12-21 15:20:27 -0800

    more work on the b120 connection

--- R/b120.R
+++ R/b120.R
@@ -1,51 +1,48 @@
 #' @title Download b120 Data
 #' @description Download Bulletin 120 Data from CDEC. This interface just grabs the latest
 #' version, parses it and returns a tibble.
-b120 <- function(location=NULL) {
+b120 <- function(location) {
   base_url <- cdec_urls$b120
 
   html_in_mem <- xml2::read_html(base_url)
   xml_text <- rvest::html_nodes(html_in_mem, "pre")
   raw_text <- rvest::html_text(xml_text)
+  text_by_line <- unlist(strsplit(raw_text, "\n"))
+
+  b120_header <- parse_b120_header(text_by_line[9])
+
+  start_end <- table_start_end(text_by_line, location)
+
+  location_raw <- text_by_line[start_end[1]:start_end[2]]
+
+  location_raw
 }
 
 
 # INTERNAL
 
-b120_parser <- function(raw_text) {
-  for (i in seq_len(length(raw_text))) {
-    line <- raw_text[i]
-    if (is_skippable(line)) next
-    if (is_table_name(line)) {
-      location_name <-
-        unlist(strsplit(line, " "))[nzchar(unlist(strsplit(line, " ")))][1:2]
-    } else {
-      split_line <- unlist(strsplit(line, " "))[nzchar(unlist(strsplit(line, " ")))]
-      args_n <- length(split_line)
-    }
-  }
+table_start_end <- function(raw_txt, location) {
+  start <- which(grepl(location, raw_txt))
+  end <- start + 3
+
+  return(c(start, end))
 }
 
-is_skippable <- function(ch) {
-  if (!nzchar(ch))
-    return(TRUE)
 
-  skippable_regex <- "--$|^B120|^DEPARTMENT|^WATER SUPPLY|^California Cooperative"
-  grepl(skippable_regex, ch)
-}
+strsplit(loc_text, "  ")
 
-is_table_name <- function(ch) {
-  header_regex <- ".+River.+|.+Lake.+"
-  grepl(header_regex, ch)
-}
+parse_b120_header <- function(line) {
+  line_content <- line[grepl("[A-Za-z]{3}|[0-9]+", line)]
+  line_dates <- line_content[!grepl("Avg", line_content)]
+  line_m <- as.data.frame(matrix(line_dates, byrow = TRUE, ncol = 2))
 
-is_table_header <- function(ch) {
-
-}
-
-b120_parse_header <- function(ch) {
+  purrr::map_chr(seq_len(nrow(line_m)), function(i) {
+    paste(line_m[i, ]$V1, line_m[i, ]$V2)
+  })
 
 }
 
 
 
+
+x[!stringr::str_detect(x, "Avg")]
