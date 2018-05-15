1c75d5808fe5ead94e4587a8e302d129abc521bf
ergz <erodriguez@flowwest.com>
2018-05-15 09:11:40 -0700

    updating pieces before cran release

--- vignettes/CDECRetrieve.R
+++ vignettes/CDECRetrieve.R
@@ -27,10 +27,39 @@ cdec_stations(river_basin = "sf bay")
 cdec_stations(county = "tehama")
 
 
+## ----eval=FALSE----------------------------------------------------------
+#  library(magrittr)
+#  library(leaflet)
+#  
+#  cdec_stations(county = "tehama") %>%
+#    map_stations()
+
+## ----eval=FALSE----------------------------------------------------------
+#  d <- cdec_stations(county = "tehama")
+#  leaflet(d) %>%
+#    addTiles() %>%
+#    addCircleMarkers(label=~station_id) #psk is way off here
+
+## ------------------------------------------------------------------------
+station <- "sha"
+cdec_datasets("sha")
+
 ## ------------------------------------------------------------------------
 library(magrittr)
-library(leaflet)
 
-cdec_stations(county = "tehama") %>% 
-  map_stations()
+cdec_datasets("sha") %>% 
+  dplyr::filter(grepl("storage", sensor_name))
+
+## ----message=FALSE-------------------------------------------------------
+sha_storage_daily <- cdec_query(station = "sha", sensor_num = "15", 
+                                dur_code = "d", start_date = "2018-01-01", 
+                                end_date = Sys.Date())
+
+sha_storage_daily
+
+## ----warning=FALSE-------------------------------------------------------
+library(ggplot2)
+
+sha_storage_daily %>% 
+  ggplot(aes(datetime, parameter_value)) + geom_line()
 
