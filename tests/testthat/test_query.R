92cbf04028e7d6780e5d194643e2990ddc00cedf
ergz <e.emanuel.rodriguez@gmail.com>
2020-12-10 20:56:33 -0800

    updates tests to 2020 just to keep them nice and short

--- tests/testthat/test_query.R
+++ tests/testthat/test_query.R
@@ -2,12 +2,12 @@ context("CDEC Query")
 library(CDECRetrieve)
 
 test_that("a response from CDEC happens", {
-  d <- cdec_query("kwk", "20", "h", "2018-03-01")
+  d <- cdec_query("kwk", "20", "h", "2020-03-01")
   expect_gt(nrow(d), 0)
 })
 
 test_that("the colnames are correct", {
-  d <- cdec_query("kwk", "20", "h", "2018-03-01")
+  d <- cdec_query("kwk", "20", "h", "2020-03-01")
   expect_equal(colnames(d), c("agency_cd", "location_id", "datetime", "parameter_cd", "parameter_value"))
 })
 
