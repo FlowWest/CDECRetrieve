358b8e7e47a643f6c00bfe7f6690a467b3a87bd9
ErinCain <ecain@flowwest.com>
2021-05-19 11:48:09 -0700

    Adds tests for rt functions

--- tests/testthat/test-rt-functions.R
+++ tests/testthat/test-rt-functions.R
@@ -1,7 +1,11 @@
-# EXAMPLE VCR USAGE: RUN AND DELETE ME
-
-foo <- function() crul::ok('https://httpbin.org/get')
+library(testthat)
 
 test_that("cdec_rt() errors when a station does not have a rating table", {
   expect_error(cdec_rt("EMM"), "rating table not found for: EMM")
 })
+
+test_that("cdec_rt() returns a df when a station has a rating table", {
+  expect_is(cdec_rt("ABJ"), c("tbl_df", "tbl", "data.frame"))
+})
+
+
